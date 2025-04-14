9b34af2633d96c414c376e85526a3d1bd15e4c81
annguyen-1707 <gasoqua1707@gmail.com>
2025-04-14 23:12:18 +0700

    done detail product

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.laptopshop.controller.client;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,17 +8,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import vn.hoidanit.laptopshop.domain.Product;
 import vn.hoidanit.laptopshop.service.ProductService;
 
-public class ProductController {
+@Controller
+public class ItemController {
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+    public ItemController(ProductService productService) {
         this.productService = productService;
     }
 
-    @GetMapping(value = "/client/product/{id}")
-    public String getProductDetailPage(Model model, @PathVariable long id) {
+    @GetMapping(value = "/product/{id}")
+    public String getProductPage(Model model, @PathVariable long id) {
         Product product = this.productService.getProductById(id);
         model.addAttribute("product", product);
-        return "client/product/view-detail";
+        return "client/product/detail";
     }
 }
